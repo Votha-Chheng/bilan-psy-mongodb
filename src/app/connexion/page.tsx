5097168085c1014363a9dcd59325de3b85c63c74
Votha-Chheng <votha_chheng@gmx.fr>
2025-05-14 00:33:28 +0200

    Build passed

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -2,6 +2,7 @@ import SignInForm from '@/components/authentication/SignInForm'
 import React from 'react'
 
 const ConnexionPage = () => {
+  console.log(process.env.DATABASE_URL)
   return (
     <div className='w-full h-screen flex justify-center items-center gap-5'>
       <SignInForm/>
